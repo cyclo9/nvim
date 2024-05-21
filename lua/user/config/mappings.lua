6c3f11db9330bf1875db6edd140e5675cd81499d
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2024-05-21 04:01:02 -0700

    Update mapping

--- lua/user/config/mappings.lua
+++ lua/user/config/mappings.lua
@@ -19,3 +19,6 @@ keymap("n", "<C-i>", "<C-w>k", { desc = "Switch to upper pane", silent = true })
 keymap("n", "<C-j>", "<C-w>h", { desc = "Switch to left pane", silent = true })
 keymap("n", "<C-k>", "<C-w>j", { desc = "Switch to lower pane", silent = true })
 keymap("n", "<C-l>", "<C-w>l", { desc = "Switch to right pane", silent = true })
+
+-- Editing
+keymap("n", "U", "<C-r>", { desc = "Redo", silent = true })
