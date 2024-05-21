5544a35bdbc4e37c68960c89ab124541fa456b5e
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2024-05-21 02:44:08 -0700

    Configured LSP

---
+++ lua/user/plugins/lsp-config.lua
@@ -0,0 +1,31 @@
+local servers = {
+    "lua_ls",
+    "pyright"
+}
+
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = servers
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            
+            for _, lsp in ipairs(servers) do
+                lspconfig[lsp].setup({})
+            end
+        end
+    },
+}
