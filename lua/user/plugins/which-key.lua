51f3465fa0c922dc39b37a50107dea65ba1307ca
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2024-05-21 21:37:17 -0700

    Update mappings

--- lua/user/plugins/which-key.lua
+++ lua/user/plugins/which-key.lua
@@ -1,30 +1,31 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-    end,
-    config = function()
-        local wk = require("which-key")
-        wk.register({
-            e = { "<cmd>NvimTreeOpen<cr>", "File Explorer" },
-            f = { 
-                name = "Telescope",
-                f = { "<cmd>Telescope find_files<cr>", "Find files" }
-            },
-            k = { vim.lsp.buf.hover, "Hover" },
-            g = {
-                name = "Goto",
-                d = { vim.lsp.buf.definition, "Go to Definition" }
-            },
-            c = {
-                a = { vim.lsp.buf.code_action, "Code Actions" }
-            },
-            b = {
-                name = "Buffer",
-                d = { "<cmd>bd<cr>", "Close Buffer" }
-            }
-        }, { prefix = "<leader>" })
-    end
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+	end,
+	config = function()
+		local wk = require("which-key")
+		wk.register({
+			c = {
+				a = { vim.lsp.buf.code_action, "Code Actions" },
+			},
+			b = {
+				name = "Buffer",
+				d = { "<cmd>bd<cr>", "Close Buffer" },
+			},
+			e = { "<cmd>NvimTreeOpen<cr>", "File Explorer" },
+			f = {
+				name = "Telescope",
+				f = { "<cmd>Telescope find_files<cr>", "Find files" },
+			},
+			g = {
+				name = "Goto",
+				d = { vim.lsp.buf.definition, "Go to Definition" },
+			},
+			k = { vim.lsp.buf.hover, "Hover" },
+			s = { vim.lsp.buf.rename, "Rename Symbol" },
+		}, { prefix = "<leader>" })
+	end,
 }
