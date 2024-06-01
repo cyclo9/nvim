c177ab3c9cfc63bb052072fb60dbf60de7998486
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2024-06-01 01:00:47 -0700

    Updated options

--- lua/user/config/options.lua
+++ lua/user/config/options.lua
@@ -1,19 +1,22 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+vim.opt.foldlevel = 99
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.termguicolors = true
 
+vim.opt.termguicolors = true
 vim.cmd("highlight LineNr guifg=#ffffff")
 
-vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
-	pattern = "*",
-	callback = function()
-		vim.defer_fn(function()
-			vim.cmd("normal zR")
-		end, 50)
-	end,
-})
+-- vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
+-- 	pattern = "*",
+-- 	callback = function()
+-- 		vim.defer_fn(function()
+-- 			vim.cmd("normal zR")
+-- 		end, 50)
+-- 	end,
+-- })
