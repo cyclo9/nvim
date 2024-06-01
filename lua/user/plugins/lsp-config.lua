634e2ff59f4ad798fbfa7d4a7bb91c4a57bab591
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2024-06-01 00:11:57 -0700

    Updated lspconfig

--- lua/user/plugins/lsp-config.lua
+++ lua/user/plugins/lsp-config.lua
@@ -1,35 +1,36 @@
 local servers = {
-    lua_ls = {},
-    pyright = {},
-    kotlin_language_server = { cmd = { "kotlin-language-server" } },
+	lua_ls = {},
+	pyright = {},
+	kotlin_language_server = { cmd = { "kotlin-language-server" } },
+	gopls = {},
 }
 
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = vim.tbl_keys(servers),
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = vim.tbl_keys(servers),
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            for lsp, config in pairs(servers) do
-                config.capabilities = capabilities
-                lspconfig[lsp].setup(config)
-            end
-        end,
-    },
+			for lsp, config in pairs(servers) do
+				config.capabilities = capabilities
+				lspconfig[lsp].setup(config)
+			end
+		end,
+	},
 }
