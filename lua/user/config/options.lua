1c6c07fc8f5ce1c57f80a5c06703d140edf7ba6f
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2024-06-06 18:05:52 -0700

    Connected nvim register to system clipboard

--- lua/user/config/options.lua
+++ lua/user/config/options.lua
@@ -12,11 +12,4 @@ vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.termguicolors = true
 vim.cmd("highlight LineNr guifg=#ffffff")
 
--- vim.api.nvim_create_autocmd({ "BufReadPost", "FileReadPost" }, {
--- 	pattern = "*",
--- 	callback = function()
--- 		vim.defer_fn(function()
--- 			vim.cmd("normal zR")
--- 		end, 50)
--- 	end,
--- })
+vim.opt.clipboard:append("unnamedplus")
