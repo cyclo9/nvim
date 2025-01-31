23ef46067dc389fb49facb2ade317de05d5d2606
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2025-01-30 22:19:01 -0800

    Switch python formatter; blue -> black

--- lua/user/plugins/conform.lua
+++ lua/user/plugins/conform.lua
@@ -4,7 +4,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				python = { "blue" },
+				python = { "black" },
 				javascript = { "prettier" },
 			},
 			format_on_save = {
