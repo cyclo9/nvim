70fdd205d75727a2490f20e3f7d36f6254c1ba78
cyclo9 <32120227+cyclo9@users.noreply.github.com.>
2025-09-29 00:04:57 -0700

    revert theme from oxocarbon to astrodark

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -11,7 +11,7 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "oxocarbon",
+    colorscheme = "astrodark",
     background = 'dark',
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
