c8c5424fcd805123e1b7c488b8e65efc5b2c2340
cyclo9 <32120227+cyclo9@users.noreply.github.com.>
2025-09-30 00:33:21 -0700

    add new mappings

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -5,11 +5,13 @@ return {
     opts = {
       mappings = {
         n = {
+          ['<leader>\\'] = { '<cmd>noh<cr>', desc='Clear highlighting' },
           -- Navigation
           ['0'] = { function () vim.api.nvim_feedkeys('^', 'n', false) end, desc='Jump to the first non-blank character of the line' },
           ['-'] = { function () vim.api.nvim_feedkeys('g_', 'n', false) end, desc='Jump to the last non-blank character of the line' },
-          --
-          ['<leader>\\'] = { '<cmd>noh<cr>', desc='Clear highlighting' }
+          -- Editing
+          ['U'] = { '<C-r>', desc='Redo' },
+          ['<leader>s'] = { function() vim.lsp.buf.rename() end, desc = "Rename symbol" }
         },
         v = {
           ['0'] = { function () vim.api.nvim_feedkeys('^', 'v', false) end, desc='Jump to the first non-blank character of the line' },
